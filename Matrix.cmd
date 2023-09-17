20370e64b81bd7ba62e8430f3e3e895c4ceaf1ce
Jimputinfn <89787903+Jimputinfn@users.noreply.github.com>
2023-09-17 16:41:34 +0300

    Add files via upload

---
+++ Matrix.cmd
@@ -0,0 +1,12 @@
+echo off
+cls
+:Matrix
+title Matrix V1
+title xirtaM
+color a
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto Matrix
