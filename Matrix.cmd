c29c762b3056b6532a71d3db054d28efa9b1e361
Jimputin <89787903+Jimputinfn@users.noreply.github.com>
2024-01-31 17:29:54 +0200

    Removed title thingy

--- Matrix.cmd
+++ Matrix.cmd
@@ -1,9 +1,8 @@
 echo off
 cls
 :Matrix
-title Matrix V1
-title xirtaM
-color a
+title Matrix V1
+color a0
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
